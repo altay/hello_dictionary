229c0c29026f3ebc1bc4f8abdc2be5c14ce37c45
altay <aguvench@gmail.com>
2010-06-23 11:43:11 -0700

    consolidated actions

--- app/controllers/wordnik_controller.rb
+++ app/controllers/wordnik_controller.rb
@@ -4,21 +4,20 @@ class WordnikController < ApplicationController
     # initialize_wordnik will set up the @wordnik object, which we can then use to call the wordnik api.
     # it also fetches the word of the day, @wotd
     # don't forget to specify your WORDNIK_API_KEY at the bottom of config/environment.rb!
-    initialize_wordnik 
-  end
-
-  def lookup
     initialize_wordnik
-    if params[:commit]=="I'm Feeling Wordie"
-      @word = @wordnik.random_word
-    else
-      @word = Word.find(params[:word_search])
+
+    if params[:commit] # user has submitted the form
+      if params[:commit]=="I'm Feeling Wordie"
+        @word = @wordnik.random_word
+      else
+        @word = Word.find(params[:word_search])
+      end
+      @wordstring = @word.wordstring
+      @definitions = @word.definitions
+      @examples = @word.examples
+      @phrases = @word.phrases
+      @related = @word.related
     end
-    @wordstring = @word.wordstring
-    @definitions = @word.definitions
-    @examples = @word.examples
-    @phrases = @word.phrases
-    @related = @word.related
   end
 
 end
